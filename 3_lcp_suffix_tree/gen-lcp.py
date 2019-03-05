d9f51f7c1c2e06ad76d237501c7f735e6a832fdf
Carl M. Kobel <kobel@pm.me>
2019-03-05 15:15:13 +0100

    ready to construct tree...

--- 3_lcp_suffix_tree/gen-lcp.py
+++ 3_lcp_suffix_tree/gen-lcp.py
@@ -1,3 +1,5 @@
+# Author: Carl M. Kobel 2019
+
 # Write a program, gen-lcp, that uses the construction algorithm from the previous algorithm to generate a suffix tree. 
 # Then use this suffix tree to output a file that contains the suffix indices in lexicographical order and the LCP 
 # array.
@@ -8,7 +10,7 @@ def sorted_suffixes(root):
         for child in root:
             child.children.sort()
 
-        for child in o.tree:
+        for child in root:
             if len(child.children) == 0:
                 yield child.string_label, child.start_index
 
@@ -20,25 +22,19 @@ def lcp(root):
     old_suffix = ''
     for suffix, start_index in sorted_suffixes(root):
         lcp = 0
-        #print('suffix: \'', suffix, '\'', sep = '')
-        #print('', len(old_suffix), 'char(s) to compare:')
-        
         for char_idx in range(len(old_suffix)): # old_suffix is mostly shorter
-            #print('  comparing', suffix[char_idx:char_idx+1], 'and', old_suffix[char_idx:char_idx + 1])
             if suffix[char_idx:char_idx+1] != old_suffix[char_idx:char_idx + 1]:
-                #print('breaking')
                 break
             lcp += 1
-
         old_suffix = suffix
-        #print(start_index, '   lcp:', lcp)
         yield (start_index, lcp)
-
-
+        
 
 if __name__ == '__main__':
-    S = 'abbaab'
+    S = 'banana'
     o = st.suffixtree(S, show = True) 
     
-    for sa, lcp in lcp(o.tree):
-        print(sa, lcp)
+    print('sa\tlcp\tstr')
+    print('~~~~~~~~~~~~~~~~')
+    for (str, sa0), (sa, lcp) in zip(sorted_suffixes(o.tree), lcp(o.tree)):
+        print(f'{sa+1}\t{lcp}\t{str}')
---
+++ 3_lcp_suffix_tree/search-st2.py
@@ -0,0 +1,8 @@
+# Author: Carl M. Kobel
+#Finally, write a program, search­st2, where you have replaced the suffix tree construction algorithm that you wrote in
+#the previous project with the new construction algorithm.
+#The program should support both generating and serialising a suffix tree and search for reads using it.
+
+
+
+
