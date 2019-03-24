0df7fd47b7a589dfef9519bd9e9324718db65f18
Carl M. Kobel <kobel@pm.me>
2019-03-25 00:18:03 +0100

    from this point on, I will use the parent stack instead of curr_node'

--- 3_lcp_suffix_tree/search-st2.py
+++ 3_lcp_suffix_tree/search-st2.py
@@ -27,14 +27,19 @@ print('i\tsa\tlcp\tsuffixes\n~~+~~~~~~~~')
 for i, suffix in enumerate(suffixes):
     print(i, sa[i], lcp[i], suffixes[i], sep = '\t')
 
+    # Case 0: lcp is zero.
     if lcp[i] == 0:
         splitcounter = 0
-        curr_node = root
+        curr_node = root # This line is strictly not necessary, but it makes the whole lot easier to understand.
         new_node = trienode(suffix, suffix)
         curr_node.adopt(new_node)
+        
         curr_node = new_node
+        parent_stack = [curr_node]
+        
         #print(' inserting at root')
     
+    # Case 1: lcp has increased.
     elif i > 0 and lcp[i] > lcp[i-1]: # lcp has increased
         curr_node.split(lcp[i]-splitcounter) # i=4: den ved ikke at der allerede er blevet splittet. Derfor splitter den en position for højt.
         splitcounter += lcp[i]
@@ -43,16 +48,26 @@ for i, suffix in enumerate(suffixes):
         new_node = trienode(suffix[lcp[i]:], curr_node.string_label + suffix[lcp[i]:])
         curr_node.adopt(new_node)
          
-    
-
+    # Case 2: lcp is the same.
     elif lcp[i] == lcp[i-1]: # lcp is the same, append to the same parent.
         new_node = trienode(suffix[lcp[i]:], curr_node.string_label + suffix[lcp[i]:])
         curr_node.adopt(new_node)
 
         if lcp[i+1] > lcp[i]: # because the next suffix has a larger lcp, we know that it is going to be appenden to this new node.
             curr_node = new_node
-
+            parent_stack.append(curr_node)
         # Do not change parents.
+
+    # Case 3: lcp is lower.
+    # I'm excited to find out, if it is at all possible to make this work without a parent pointer?
+    # I have to traverse upwards, and split nodes. Doesn't sound like it is possible??
+    # Hvad, hvis jeg antager at lcp kun kan stige med en for hver iteration? Det er selvfølgelig en selvopfyldende profeti at det kommer til at virke fordi dette eksempel ikke har andre cases..
+    # Nu ved jeg at jeg er nødt til at lave en forældrekø.
+
+
+
+
+
     
 
 
@@ -60,7 +75,7 @@ for i, suffix in enumerate(suffixes):
 
 
 
-    root.visualize(f'iter/{i}')
+    root.visualize(f'iter/{i} {suffix}')
 
 
 # Jeg tror jeg spilder rigtig meget tid på ikke bare at have en parent pointer. Det vil kun tilføje konstant tid, men gør det noget?
