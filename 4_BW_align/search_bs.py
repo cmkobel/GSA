039587da6f09496f75931c8e527f15d7bef5db0e
Carl M. Kobel <kobel@pm.me>
2019-04-04 20:04:51 +0200

    finder alle positioner, klar til oprydning

--- 4_BW_align/search_bs.py
+++ 4_BW_align/search_bs.py
@@ -5,36 +5,56 @@ from math import ceil, log2
 class search_bs:
     def __init__(self, S):
         self.S = S
-        self.sa = naive_sa.sa(S)[0]
+        self.sa, self.sa_str = naive_sa.sa(S)
 
-    def find_start_pos(self, p):
+    def find_start_position(self, pattern):
+        """ The position returned by this function should be the first of the suffix arrays that equals. 
+            This function could be put instide find_positions. """
+
         j = -1
-        L = 0
-        R = len(S) - 1
-        maximum_iterations = int(ceil(log2(len(S))))
-        for i in range(maximum_iterations):
-            M = -(-(R+L)//2) # ceiling integer division
-            m_str = S[self.sa[M]:self.sa[M]+len(p)]
+        left = 0
+        right = len(S) - 1
+        theoretical_max = int(ceil(log2(len(S))))
+        for i in range(theoretical_max):
+            middle = -(-(right+left)//2) # ceiling integer division
+            middle_string = S[self.sa[middle]:self.sa[middle]+len(pattern)]
 
-            if p == m_str:
-                j = self.sa[M]
-            elif p > m_str:
-                L = M
-            elif p < m_str:
-                R = M-1
+            if pattern == middle_string:
+                j = self.sa[middle]
+            elif pattern > middle_string:
+                left = middle
+            elif pattern < middle_string:
+                right = middle-1 # Because the pattern is in the upper part, we can exclude the lower.
             
             if j != -1:
                 break
 
-        print(j)
 
-S = 'noteuhsnatoheisthaosneuthsaonteidstaoheusnthaoseintdsanoteuhsnatoesitdasoethntaheduntahoudntaoheduthedunitaonstuehontehuidsantoehunst'
+        return middle # The index of SA that contains the matching string.
+
+    def find_positions(self, pattern):
+        start_position = self.find_start_position(pattern)
+        positions = []
+
+        for i in self.sa[start_position:]:
+            #print(i, S[i:i+len(pattern)])
+            if S[i:i+len(pattern)] == pattern:
+                positions.append(i)
+            else:
+                break
+
+        return [(i, S[i:i+len(pattern)]) for i in positions]
+
+
+
+
+
+
+S = 'mississippi'
 
 o = search_bs(S)
-for i in range(len(S)):
-    string = S[i:]
-    print(string, end = ' ')
-    o.find_start_pos(string)
-o.find_start_pos(''.join([i for i in reversed(S)]))
 
-# Når startpositionen er fundet, skal vi fortsætte indtil de næstkommede naboer ikke længere indeholder det samme.
+positions = o.find_positions('iss')
+
+print( positions)
+
