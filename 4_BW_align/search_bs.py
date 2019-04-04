6de1b86bf348ee991cccd557242e8ed112f5d6ec
Carl M. Kobel <kobel@pm.me>
2019-04-04 17:44:39 +0200

    cleanup

--- 4_BW_align/search_bs.py
+++ 4_BW_align/search_bs.py
@@ -1,6 +1,5 @@
 import naive_sa
-from math import log2
-
+from math import ceil, log2
 
 
 class search_bs:
@@ -8,48 +7,28 @@ class search_bs:
         self.S = S
         self.sa = naive_sa.sa(S)[0]
 
-
     def find_start_pos(self, p):
         j = -1
         L = 0
         R = len(S) - 1
-        # print('sa')
-        # print('==========')
-        # for _i, i in enumerate(self.sa):
-        #     print(f'{_i}: {i} {S[i:]}')
-        # print('==========')
-
-        #while True:
-        for i in range(int(log2(len(S))+2)):
+        maximum_iterations = int(ceil(log2(len(S))))
+        for i in range(maximum_iterations):
             M = -(-(R+L)//2) # ceiling integer division
             m_str = S[self.sa[M]:self.sa[M]+len(p)]
 
-
-
-
             if p == m_str:
                 j = self.sa[M]
-
-
             elif p > m_str:
                 L = M
-
             elif p < m_str:
-
                 R = M-1
-
             
             if j != -1:
                 break
 
-
-
         print(j)
 
-
-
 S = 'noteuhsnatoheisthaosneuthsaonteidstaoheusnthaoseintdsanoteuhsnatoesitdasoethntaheduntahoudntaoheduthedunitaonstuehontehuidsantoehunst'
-
 
 o = search_bs(S)
 for i in range(len(S)):
