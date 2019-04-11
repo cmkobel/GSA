babe2c49e208865e3c5cf522afec2f18694d3368
Carl M. Kobel <kobel@pm.me>
2019-04-11 15:09:26 +0200

    classificeret

--- 4_BW_align/bwt.py
+++ 4_BW_align/bwt.py
@@ -1,97 +1,125 @@
 import naive_sa
 from t4 import t4_genome as t4
 
-#S = t4()
-S = 'Mississippi'
-sentinel = '$'
-S = S.lower() + sentinel
-sa, sa_str = naive_sa.sa(S)
-alphabet = sorted(set(S))
+
+class bwt:
+    def __init__(self, S):
+
+        self.S = S.lower() + '$'
+        self.sa, self.sa_str = naive_sa.sa(self.S)
+        self.alphabet = sorted(set(self.S))
+
+        for _i, (i, j) in enumerate(zip(self.sa, self.sa_str)):
+            #print(_i, i, j)
+            pass
+
+        # Routine:
+        self.bwt = self.compute_bwt()
+        
+        self.C_table = [i for i in self.compute_C_table()]
+
+        self.O = self.compute_O_table()
 
 
-def compute_bwt():
-    return [S[i-1] for i in sa]
+        self.inv_alph = {j: i for i, j in enumerate(self.alphabet)} # reverse lookup in alphabet f('$') = 0
 
 
-bwt = compute_bwt()
 
 
-def compute_C_table():
-    counts = {}
-    # Set counts-table to zero.
-    for i in alphabet:
-        counts[i] = 0
-
-    # Count occurences of letters
-    for i in S:
-        counts[i] +=1
-
-    # Calc. prefix sum.
-    for i in counts:
-        if i == alphabet[0]:
-            yield 0
-        elif i == alphabet[1]:
-            yield 0
-            prev = counts[i]
-        else:
-            yield prev
-            prev += counts[i]
+    def compute_bwt(self):
+        return [self.S[i-1] for i in self.sa]
 
 
-C_table = [i for i in compute_C_table()]
+    def compute_C_table(self):
+        counts = {}
+        # Set counts-table to zero.
+        for i in self.alphabet:
+            counts[i] = 0
+
+        # Count occurences of letters
+        for i in self.S:
+            counts[i] +=1
+
+        # Calc. prefix sum.
+        for i in counts:
+            if i == self.alphabet[0]:
+                yield 0
+            elif i == self.alphabet[1]:
+                yield 0
+                prev = counts[i]
+            else:
+                yield prev
+                prev += counts[i]
 
 
-def compute_O_table():
-    O = [[] for i in range(len(S))]
+    def compute_O_table(self):
+        O = [[] for i in range(len(self.S))]
 
-    row = [0 for i in range(len(C_table))]
-    for _i, i in enumerate(bwt):
+        row = [0 for i in range(len(self.C_table))]
+        for _i, i in enumerate(self.bwt):
 
-        row[alphabet.index(i)] += 1
-        O[_i] = [i for i in row]
+            row[self.alphabet.index(i)] += 1
+            O[_i] = [i for i in row]
 
-    return O
-
-
-O = compute_O_table()
+        return O
 
 
-# Helper funcs/vars for O and C-table
-inv_alph = {j: i for i, j in enumerate(alphabet)} # reverse lookup in alphabet f('$') = 0
-def access_O(char, idx):
-    return O[idx][inv_alph[char]]
 
-def present_O():
-    """ Pretty prints the O-table for debug. 
-        mississippi$
-            i   p   s   s   m   $   p   i   s   s   i   i   
-        $   0   0   0   0   0   1   1   1   1   1   1   1   
-        i   1   1   1   1   1   1   1   2   2   2   3   4   
-        m   0   0   0   0   1   1   1   1   1   1   1   1   
-        p   0   1   1   1   1   1   2   2   2   2   2   2   
-        s   0   0   1   2   2   2   2   2   3   4   4   4   
-    """
-    print(S)
-    print('\t', end = '')
-    for char in bwt:
-        print(char, '\t', end = '')
-    print()
-    for char in alphabet:
-        print(char, '\t', end = '')
-        for i in range(len(S)):
-            print(access_O(char, i), '\t', end = '')
+    # Helper funcs/vars for O and C-table
+    
+    def access_O(self, char, idx):
+        return self.O[idx][self.inv_alph[char]]
+
+    def present_O(self):
+        """ Pretty prints the O-table for debug. 
+            mississippi$
+                i   p   s   s   m   $   p   i   s   s   i   i   
+            $   0   0   0   0   0   1   1   1   1   1   1   1   
+            i   1   1   1   1   1   1   1   2   2   2   3   4   
+            m   0   0   0   0   1   1   1   1   1   1   1   1   
+            p   0   1   1   1   1   1   2   2   2   2   2   2   
+            s   0   0   1   2   2   2   2   2   3   4   4   4   
+        """
+        print(self.S)
+        print('\t', end = '')
+        for char in self.bwt:
+            print(char, '\t', end = '')
+        print()
+        for char in self.alphabet:
+            print(char, '\t', end = '')
+            for i in range(len(self.S)):
+                print(self.access_O(char, i), '\t', end = '')
+            print()
+
         print()
 
-present_O()
-#print(access_O('p', 1))
+    def find_positions(self, pattern):
+        print(self.S)
+
+
+        L = 0
+        R = len(self.S)-1
+        i = len(pattern)-1
+
+        while i >= 0 and L <= R:
+            # compute L(w[i...m]) from L(w[i+1...m])
+            # if L == 0:
+            #     L = self.C_table[self.inv_alph[pattern[i]]] + 1
+            # else:
+            L = self.C_table[self.inv_alph[pattern[i]]] + self.access_O(pattern[i], L-1) * (L != 0) + 1
+
+            # compute R(w[i...m]) from R(w[i+1...m])
+            R = self.C_table[self.inv_alph[pattern[i]]] + self.access_O(pattern[i], R)
+            i -= 1
+
+        if i < 0 and L <= R:
+            print(L, R,[i for i in range(L, R+1)])
+            return [str(self.sa_str[i]) for i in range(L, R+1)]
+        else:
+            print(L, R, [i for i in range(L, R+1)])
+            return []
 
 
 
-
-
-
-
-
-
-
-
+o = bwt('mississippi')
+print(o.find_positions('i'))
